14e88725de88ea3a9a66aa7bcd17c03fc58c2e84
Paul Thomas <traitorpt@gmail.com>
2020-01-05 14:31:44 -0800

    updated serialize-javascript

---
+++ models/Stats.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const statsSchema = new Schema({
+  miles: { type: Number, required: true },
+  calories: { type: Number, required: true },
+  time: { type: Number, required = true }
+})
+
+const Stats = mongoose.model("Saved", statsSchema );
+
+module.exports = Stats;
